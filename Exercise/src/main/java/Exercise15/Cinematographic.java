af4bbffceec24477f3d8a2257b062be8ebdc64e0
Juanes07 <juanesdm3@gmail.com>
2022-04-19 23:02:52 -0500

    Se agrega la clase Menu donde esta alojado el metodo ShowMenu el cual muestra el menu de opciones al usuario. Se eliminan los archivos info, movies y character debido a un error en el entendimiento del ejercicio.

--- Exercise/src/main/java/Exercise15/Cinematographic.java
+++ Exercise/src/main/java/Exercise15/Cinematographic.java
@@ -1,9 +1,35 @@
 package Exercise15;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Cinematographic {
     public static void main(String[] args) {
-        Movies movies = new Movies();
+        Scanner scanner = new Scanner(System.in);
+        Menu menu = new Menu();
+        int menuOption;
+        do {
+            menu.showMenu();
+            menuOption = scanner.nextInt();
+            switch (menuOption){
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+                default:
+                    System.out.println("");
+            }
+        } while (menuOption <8);
+        menu.closeMenu();
     }
 }
---
+++ Exercise/src/main/java/Exercise15/Menu.java
@@ -0,0 +1,24 @@
+package Exercise15;
+
+import java.util.ArrayList;
+
+public class Menu {
+
+    public void showMenu(){
+        System.out.println("Opciones del sistema:" + "\n"+
+                "1.Nuevo Actor o Actriz"+ "\n"+
+                "2.Buscar Actor o Actriz"+ "\n"+
+                "3.Eliminar Actor o Actriz"+ "\n"+
+                "4.Modificar Actor o Actriz" + "\n"+
+                "5.Ver todos los Actores o Actrices"+ "\n"+
+                "6.Ver peliculas de los actores"+ "\n"+
+                "7.Ver categoria de las peliculas" + "\n"+
+                "8.Salir del sistema");
+    }
+
+    void closeMenu () {
+        System.out.println("vuelva pronto");
+    }
+
+
+}
